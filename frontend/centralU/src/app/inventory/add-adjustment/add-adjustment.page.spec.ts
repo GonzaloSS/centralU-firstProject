9064557e436cf62d3a93618ca9a884e732170075
Nestor <maquinanestor@gmail.com>
2021-03-04 13:28:36 +0000

    tests

--- frontend/centralU/src/app/inventory/add-adjustment/add-adjustment.page.spec.ts
+++ frontend/centralU/src/app/inventory/add-adjustment/add-adjustment.page.spec.ts
@@ -52,4 +52,32 @@ describe('AddAdjustmentPage', () => {
   
       expect(component.onFormSubmit()).toEqual(inventory);
     });
+
+    it('should not create adjustment', ()=>{
+      let  inventory = {
+          id: null,
+          name: "inventory test",
+          location_ids: 1,
+          product_ids: 1,
+          accounting_date: "1111-01-01",
+          exhausted: 1,
+          prefill_counted_quantity: "inventory test",
+          date:null,
+          state:null,
+          product_name: null,
+          location_name: null
+        };
+    
+        component.inventoryForm.controls['name'].setValue(inventory.name);
+        component.inventoryForm.controls['location_ids'].setValue(inventory.location_ids);
+        component.inventoryForm.controls['product_ids'].setValue(inventory.product_ids);
+        component.inventoryForm.controls['accounting_date'].setValue(inventory.accounting_date);
+        component.inventoryForm.controls['exhausted'].setValue(inventory.exhausted);
+        component.inventoryForm.controls['prefill_counted_quantity'].setValue(inventory.prefill_counted_quantity);
+  
+        component.onFormSubmit();
+    
+    
+        expect(component.onFormSubmit()).not.toEqual(inventory);
+      });
 });
